19afa5b94d606c84d8164e9a1a90bebb5e2f44d6
Marwan Alaloosi <manoalosi@gmail.com>
2018-12-04 13:50:41 -0600

    Drill one finished

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 function max(numbers) {
     let maxNum = numbers[0]
-    let i =0;
+    let i = 0
     while (i < numbers.length) {
         if (numbers[i] > maxNum) {
             maxNum = numbers[i];
-            i++
         }
+        i++
     }
     return maxNum;
 }
 max([1, 2, 3, 4, 5])
 
-function min(numbers){
-    let minNum = numbers[0]
-    let i = 0;
-    while (i < numbers.length){
-        if(numbers[i] < minNum){
-            minNum = numbers[i]
+    function min(numbers){
+        let minNum = numbers[0]
+        let i = 0;
+        while (i < numbers.length){
+            if(numbers[i] < minNum){
+                minNum = numbers[i]
+            }
             i++
         }
+        return minNum;
     }
-    return minNum;
-}
+    min([1, 2, 3, 4, 5])
+
 
 function average(numbers){
     let sum = 0;
-    let i = 0;
-    while (i < numbers.length){
-        sum += numbers[i];
-        i++;
-    }
-    return sum/numbers.length;
+    numbers.forEach(function(item){
+        sum += item
+    })
+    return sum /numbers.length
 }
-
-average(1, 2, 3, 4)
+average([1, 2, 3, 4])
